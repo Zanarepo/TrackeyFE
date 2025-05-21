1d2ab955aa8f1c066c4b462d83dc76e379bfa5ed
prinzana <prinzana@gmail.com>
2025-05-21 18:36:48 +0100

    updated the unpaid supplies compo

--- src/component/DynamicSales/DynamicSales.js
+++ src/component/DynamicSales/DynamicSales.js
@@ -743,7 +743,7 @@ export default function SalesTracker() {
 
       {/* Edit Modal */}
       {editing && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50">
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50 mt-24">
           <form
             onSubmit={(e) => {
               e.preventDefault();
